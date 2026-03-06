f000ea9082aeb77ea4e2194d6b4dbee1f8876993
CauaGarcia277 <garciacaua50@gmail.com>
2026-03-06 14:57:35 -0300

    Update db.py

--- Estoque_v1/app/db.py
+++ Estoque_v1/app/db.py
@@ -2,10 +2,3 @@ import mysql.connector
 import os
 
 #Conexão banco de dados
-def conectar():
-    return mysql.connector.connect(
-        host= "localhost",
-        user= "root",
-        password= "Ca272525#",
-        database= "geren_estoque"
-    )
